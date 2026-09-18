163fcc4f775bd3c370ce650784091fdeb8aa25f1
Rajah Williams <rajahwilliams77@gmail.com>
2026-09-18 05:08:17 -0500

    Change User methods to module functions

--- server.py
+++ server.py
@@ -1,9 +1,8 @@
 import socket
 import argparse
 import re
-from echo_server import conn, addr
 
-errors = {
+ERRORS = {
     100: 'Unknown command',
     101: 'Malformed or missing arguments (includes an illegal nickname)',
     102: 'Nickname already taken',
@@ -12,94 +11,100 @@ errors = {
     105: 'Line exceeds 512 bytes'
 }
 
-class Client():
+class User:
     def __init__(self, conn, addr):
         self.conn = conn
         self.addr = addr
-        self.nickname = ''
+        self.nickname = None
         self.buffer = b''
+ 
+    def send_line(self, text):
+        data = (text + '\n').encode('utf-8', errors='replace')
+        try:
+            self.conn.sendall(data)
+        except OSError:
+            pass
+ 
+    def send_error(self, code):
+        self.send_line('ERR ' + str(code) + ' ' + str(ERRORS[code]))
 
-    def print_error(self, e):
-        if e in errors:
-            print('ERROR:', errors[e])
-
-    def ingest_lines(self):
-        while True:
-            chunk = conn.recv(4096)
-            if not chunk:
-                break
-            buffer += chunk
+def ingest_lines(conn):
+    while True:
+        chunk = conn.recv(4096)
+        if not chunk:
+            break
+        buffer += chunk
         
-            while b'\n' in buffer:
-                raw, buffer = buffer.split(b'\n', 1)
-                line = raw.rstrip(b'\r').decode('utf-8', errors='replace').strip()
-                if line:
-                    self.handle_line(line)
+        while b'\n' in buffer:
+            raw, buffer = buffer.split(b'\n', 1)
+            line = raw.rstrip(b'\r').decode('utf-8', errors='replace').strip()
+            if line:
+                handle_line(line)
 
-    def handle_line(self, line):
-        if len(line.encode('utf-8')) > 512:
-            print(errors[105])
-            return
+def handle_line(line, user):
+    if len(line.encode('utf-8')) > 512:
+        user.send_error(105)
+        return
 
-        split = line.split()
-        if split:
-            verb = split[0].upper()
-            args = split.copy().remove(verb)
+    split = line.split()
+    if split:
+        verb = split[0].upper()
+        args = split.copy().remove(verb)
 
-            match verb:
-                case 'NICK':
-                    self.handle_nick(args)
-                case 'MSG':
-                    self.handle_msg(args)
-                case 'PM':
-                    self.handle_pm(args)
-                case 'WHO':
-                    self.handle_who(args)
-                case 'QUIT':
-                    self.handle_quit(args)
-                case _:
-                    self.print_error(100)
-        else:
-            self.print_error(100)
+        match verb:
+            case 'NICK':
+                handle_nick(args)
+            case 'MSG':
+                handle_msg(args)
+            case 'PM':
+                handle_pm(args)
+            case 'WHO':
+                handle_who(args)
+            case 'QUIT':
+                handle_quit(args)
+            case _:
+                user.send_error(100)
+    else:
+        user.send_error(100)
 
-    def handle_nick(self, args):
-        if not len(args) == 1 or not re.fullmatch('[A-Za-z0-9_]', args[0]):
-            self.print_error(101)
-            return
+def handle_nick(args, user):
+    if not len(args) == 1 or not re.fullmatch('[A-Za-z0-9_]', args[0]) or not len(args[0]) > 0 and len(args[0])<= 16:
+        user.send_error(101)
+        return
 
-        if args[0] == 'name': # Make this a check if the name is in user list (Server side)
-            self.print_error(102)
-            return
+    if args[0] == 'name': # Make this a check if the name is in user list (Server side)
+        user.send_error(102)
+        return
 
-        self.nickname = args[0]
+    nickname = args[0]
 
-    def handle_msg(self, args):
-        text = args.join()
+def handle_msg(args, user):
+    text = args.join()
 
-    def handle_pm(self, args):
-        if not len(args) > 1:
-            self.print_error(101)
-            return
+def handle_pm(args, user):
+    if not len(args) > 1:
+        user.send_error(101)
+        return
 
-        text = args.copy().remove(args[0]).join()
+    if not args[0] == 'name': # Make this a check if the name is in user list (Server side)
+        user.send_error(104)
+        return
 
-        if not args[0] == 'name': # Make this a check if the name is in user list (Server side)
-            self.print_error(104)
-            return
+    text = args.copy().remove(args[0]).join()
 
-    def handle_who(self, args):
-        if not len(args) == 0:
-            self.print_error(101)
-            return
+def handle_who(args, user):
+    if not len(args) == 0:
+        user.send_error(101)
+        return
 
-        print('users') # print user list (Server side)
+    print('users') # print user list (Server side)
 
-    def handle_quit(self, args):
-        if not len(args) == 0:
-            self.print_error(101)
-            return
+def handle_quit(args, user):
+    if not len(args) == 0:
+        user.send_error(101)
+        return
 
-        print('quit') #disconnect from server
+    print('quit') #disconnect from server
 
 
 def main():
@@ -111,8 +116,20 @@ def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((args.host, args.port))
+    sock.listen(10)
 
-    client = Client()
+    ingest_lines
+
+    while True:
+        conn, addr = sock.accept()
+        print('connected:', addr)
+        try:
+            ingest_lines(conn)
+        except (ConnectionResetError):
+            pass
+        finally:
+            print('disconnected:', addr)
+            conn.close()
 
 if __name__ == '__main__':
     main()
